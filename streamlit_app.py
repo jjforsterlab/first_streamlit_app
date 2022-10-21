b4cc404452e2f349955cdeef095da4d24a3fa9dd
jjforsterlab <116078750+jjforsterlab@users.noreply.github.com>
2022-10-21 12:30:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,7 @@ def insert_row_snowflake(new_fruit):
   
 my_added_fruit = streamlit.text_input('What fruit would you like to add?')
 my_data_rows = insert_row_snowflake(my_added_fruit)
+streamlit.write(my_data_rows)
 
 
 
